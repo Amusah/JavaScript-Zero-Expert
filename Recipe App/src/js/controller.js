23f0f2b3e540d7ed1f493a91a9031d48e72da21a
Amusah <henryamusah@gmail.com>
2024-01-24 23:54:28 +0000

    Implementing search result pt 2

--- Recipe App/src/js/controller.js
+++ Recipe App/src/js/controller.js
@@ -1,10 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+
+// Hot module reloading,.....only works in parcel
+if(module.hot){
+  module.hot.accept();
+}
 
 const controlRecipes = async function(){
   try {
@@ -25,6 +31,8 @@ const controlRecipes = async function(){
 
 const controlSearchResults = async function(){
   try {
+    resultsView.renderSpinner();
+    
     // 1) Get search query
     const query = searchView.getQuery();
     if(!query) return;
@@ -33,12 +41,11 @@ const controlSearchResults = async function(){
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results)
   } catch (error) {
     console.error(error);
   }
-}
-controlSearchResults();
+};
 
 /****** implementing publisher subscriber pattern ******/
 // const init = function(){
