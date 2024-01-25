f7abf4b76ab7d840bdb947d7185c109ec475c025
Amusah <henryamusah@gmail.com>
2024-01-25 08:26:01 +0000

    Implementing pagination

--- Recipe App/src/js/controller.js
+++ Recipe App/src/js/controller.js
@@ -41,7 +41,8 @@ const controlSearchResults = async function(){
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results)
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (error) {
     console.error(error);
   }
--- Recipe App/src/js/views/resultsView.js
+++ Recipe App/src/js/views/resultsView.js
@@ -5,7 +5,7 @@ import icons from 'url:../../img/icons.svg' // parcel v2
 
 class ResultsView extends View{
   _parentElement = document.querySelector('.results');
-    _errorMessage = 'No recipes found for your query! Please try again :)';
+  _errorMessage = 'No recipes found for your query! Please try again :)';
   _successMessage = '';
 
   _generateMarkup(){
